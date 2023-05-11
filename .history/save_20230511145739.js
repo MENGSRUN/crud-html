a5d4e12728c88f9206bda898d5d0b0ec3f3ba23d
MENGSRUN <git\mengsrun448@gmail.com>
2023-05-11 15:40:38 +0700

    final

---
+++ .history/save_20230511145739.js
@@ -0,0 +1,12 @@
+//method to save data into localstorage
+function save(){
+    //get data from localstorage and store to contaclist array
+    //we must to use JSON.parse, because data as string, we need convert to array
+    contactList = JSON.parse(localStorage.getItem('listItem')) ?? []
+
+    //get last array to get last id
+    //and store it into variable id
+    var id
+    contactList.length != 0 ? contactList.findLast((item) => id = item.id) : id = 0
+
+    if(document.getElementById('id').value){
